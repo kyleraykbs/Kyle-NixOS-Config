31c78fd71fe379a01613bf3e74360f161c9f7d9d
Gemstone48 <mizztree78@gmail.com>
2023-07-26 04:15:10 -0700

    Added binds to hyprland module.

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -33,6 +33,62 @@ in
         default = config.lib.stylix.colors.base08-hex or "ffffff";
       };
     };
+    config = {
+      binds = mkOption {
+        type = types.str;
+        default = ''
+          # See https://wiki.hyprland.org/Configuring/Keywords/ for more
+          $mainMod = ALT
+
+          # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
+          bind = $mainMod, RETURN, exec, kitty
+          bind = $mainMod, C, killactive,
+          bind = $mainMod, M, exit,
+          bind = $mainMod, SPACE, togglefloating,
+          bind = $mainMod, R, exec, wofi --show drun
+          bind = $mainMod, F, fullscreen, # dwindle
+          bind = $mainMod, J, togglesplit, # dwindle
+
+          # Move focus with mainMod + arrow keys
+          bind = $mainMod, left, movefocus, l
+          bind = $mainMod, right, movefocus, r
+          bind = $mainMod, up, movefocus, u
+          bind = $mainMod, down, movefocus, d
+
+          # Switch workspaces with mainMod + [0-9]
+          bind = $mainMod, 1, workspace, 1
+          bind = $mainMod, 2, workspace, 2
+          bind = $mainMod, 3, workspace, 3
+          bind = $mainMod, 4, workspace, 4
+          bind = $mainMod, 5, workspace, 5
+          bind = $mainMod, 6, workspace, 6
+          bind = $mainMod, 7, workspace, 7
+          bind = $mainMod, 8, workspace, 8
+          bind = $mainMod, 9, workspace, 9
+          bind = $mainMod, 0, workspace, 10
+
+          # Move active window to a workspace with mainMod + SHIFT + [0-9]
+          bind = $mainMod SHIFT, 1, movetoworkspace, 1
+          bind = $mainMod SHIFT, 2, movetoworkspace, 2
+          bind = $mainMod SHIFT, 3, movetoworkspace, 3
+          bind = $mainMod SHIFT, 4, movetoworkspace, 4
+          bind = $mainMod SHIFT, 5, movetoworkspace, 5
+          bind = $mainMod SHIFT, 6, movetoworkspace, 6
+          bind = $mainMod SHIFT, 7, movetoworkspace, 7
+          bind = $mainMod SHIFT, 8, movetoworkspace, 8
+          bind = $mainMod SHIFT, 9, movetoworkspace, 9
+          bind = $mainMod SHIFT, 0, movetoworkspace, 10
+
+          # Scroll through existing workspaces with mainMod + scroll
+          bind = $mainMod, mouse_down, workspace, e+1
+          bind = $mainMod, mouse_up, workspace, e-1
+
+          # Move/resize windows with mainMod + LMB/RMB and dragging
+          bindm = $mainMod, mouse:272, movewindow
+          bindm = $mainMod, mouse:273, resizewindow
+        '';
+      };
+    };
   };
 
   config = mkIf cfg.enable {
@@ -198,55 +254,7 @@ in
       # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
 
 
-      # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-      $mainMod = ALT
-
-      # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-      bind = $mainMod, RETURN, exec, kitty
-      bind = $mainMod, C, killactive,
-      bind = $mainMod, M, exit,
-      bind = $mainMod, SPACE, togglefloating,
-      bind = $mainMod, R, exec, wofi --show drun
-      bind = $mainMod, F, fullscreen, # dwindle
-      bind = $mainMod, J, togglesplit, # dwindle
-
-      # Move focus with mainMod + arrow keys
-      bind = $mainMod, left, movefocus, l
-      bind = $mainMod, right, movefocus, r
-      bind = $mainMod, up, movefocus, u
-      bind = $mainMod, down, movefocus, d
-
-      # Switch workspaces with mainMod + [0-9]
-      bind = $mainMod, 1, workspace, 1
-      bind = $mainMod, 2, workspace, 2
-      bind = $mainMod, 3, workspace, 3
-      bind = $mainMod, 4, workspace, 4
-      bind = $mainMod, 5, workspace, 5
-      bind = $mainMod, 6, workspace, 6
-      bind = $mainMod, 7, workspace, 7
-      bind = $mainMod, 8, workspace, 8
-      bind = $mainMod, 9, workspace, 9
-      bind = $mainMod, 0, workspace, 10
-
-      # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      bind = $mainMod SHIFT, 1, movetoworkspace, 1
-      bind = $mainMod SHIFT, 2, movetoworkspace, 2
-      bind = $mainMod SHIFT, 3, movetoworkspace, 3
-      bind = $mainMod SHIFT, 4, movetoworkspace, 4
-      bind = $mainMod SHIFT, 5, movetoworkspace, 5
-      bind = $mainMod SHIFT, 6, movetoworkspace, 6
-      bind = $mainMod SHIFT, 7, movetoworkspace, 7
-      bind = $mainMod SHIFT, 8, movetoworkspace, 8
-      bind = $mainMod SHIFT, 9, movetoworkspace, 9
-      bind = $mainMod SHIFT, 0, movetoworkspace, 10
-
-      # Scroll through existing workspaces with mainMod + scroll
-      bind = $mainMod, mouse_down, workspace, e+1
-      bind = $mainMod, mouse_up, workspace, e-1
-
-      # Move/resize windows with mainMod + LMB/RMB and dragging
-      bindm = $mainMod, mouse:272, movewindow
-      bindm = $mainMod, mouse:273, resizewindow
+      ${cfg.config.binds}
 
       ### kitty
       windowrule = animation popin,^(kitty)$ # sets the animation style for kitty
