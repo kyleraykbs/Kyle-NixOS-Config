ac1e0bc505317cc6c64e6e36c002b687a3b677b6
Kyle <kyleraykbs@proton.me>
2023-08-07 13:35:50 -0700

    Fully disabled noveau.

--- hosts/baha-blast/hardware.nix
+++ hosts/baha-blast/hardware.nix
@@ -14,9 +14,28 @@
     "vfio_pci"
   ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.blacklistedKernelModules = [
-    nouveau
-  ];
+
+  boot.extraModprobeConfig = ''
+    blacklist nouveau
+    options nouveau modeset=0
+  '';
+  
+  services.udev.extraRules = lib.mkDefault ''
+    # Remove NVIDIA USB xHCI Host Controller devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+
+    # Remove NVIDIA USB Type-C UCSI devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+
+    # Remove NVIDIA Audio devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+
+    # Remove NVIDIA VGA/3D controller devices
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  '';
+
+  boot.blacklistedKernelModules = lib.mkDefault [ "nouveau" "nvidia" ];
+
   boot.extraModulePackages = [ ];
   boot.kernelParams = [ 
     "intel_iommu=on" 
