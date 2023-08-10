42fa8f4b45b1fffb2baa094aba6f1560db8d0008
Kyle <kyleraykbs@proton.me>
2023-08-09 21:22:19 -0700

    Remote managing Wolfe's machine (the bad way): Part 1. [READ DESC FOR LOG]
    Re-added vfio.

--- hosts/citadel-core/hardware.nix
+++ hosts/citadel-core/hardware.nix
@@ -28,12 +28,17 @@
       availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
     };
 
+    # 01:00.0 VGA compatible controller [0300]: NVIDIA Corporation TU104 [GeForce RTX 2080 SUPER] [10de:1e81] (rev a1)
+    # 01:00.1 Audio device [0403]: NVIDIA Corporation TU104 HD Audio Controller [10de:10f8] (rev a1)
+    # 01:00.2 USB controller [0c03]: NVIDIA Corporation TU104 USB 3.1 Host Controller [10de:1ad8] (rev a1)
+    # 01:00.3 Serial bus controller [0c80]: NVIDIA Corporation TU104 USB Type-C UCSI Controller [10de:1ad9] (rev a1)
+
     kernelParams = [ 
       "intel_iommu=on" 
       "iommu=pt"
       "pcie_acs_override=downstream,multifunction" 
       "kvm.ignore_msrs=1" 
-#      "vfio-pci.ids=10de:1e81,10de:10f8,10de:1ad8,10de:1ad9"
+     "vfio-pci.ids=10de:1e81,10de:10f8,10de:1ad8,10de:1ad9"
     ];
     kernelModules = [
       "kvm-intel"
