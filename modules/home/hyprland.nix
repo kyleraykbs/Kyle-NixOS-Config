fd02c2626e9f435f8b7929ab384b1420f69bf375
Kyle <kyleraykbs@proton.me>
2023-08-10 02:07:15 -0700

    Screen-Share fixes.

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -76,6 +76,13 @@ in
         '';
       };
 
+      debug = {
+        damage_tracking = mkOption {
+          type = types.bool;
+          default = true;
+        };
+      };
+
       dwindle = {
         pseudotile = mkOption {
           type = types.bool;
@@ -117,11 +124,6 @@ in
         stopkeybind = mkOption {
           type = types.str;
           default = "$mainMod SHIFT, A";
-        };
-
-        redraw_fix = mkOption {
-          type = types.bool;
-          default = true;
         };
 
         obskeybind = mkOption {
@@ -319,7 +321,7 @@ in
       }
 
       debug {
-        damage_tracking = ${builtins.toString (if cfg.config.screenshare.redraw_fix then 1 else 2)}
+        damage_tracking = ${builtins.toString (if cfg.config.debug.damage_tracking then 2 else 1)}
       }
 
       # Example windowrule v1
@@ -329,8 +331,8 @@ in
       # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
       ${cfg.config.binds}
 
-      ${(if cfg.config.screenshare.enable then ''bind = ${cfg.config.screenshare.keybind},exec,killall -9 wf-recorder; killall -9 ffplay; wf-recorder -f pipe:99 -m matroska -g "1920,0 1920x1080" -c libx264 -r 30 -b:v 2M -bf 0 -bufsize 512K 99>&1 >&2 | ffplay -'' else "")}
-      ${(if cfg.config.screenshare.enable then ''bind = ${cfg.config.screenshare.stopkeybind},exec,killall -9 wf-recorder; killall -9 ffplay;'' else "")}
+      ${(if cfg.config.screenshare.enable then ''bind = ${cfg.config.screenshare.keybind},exec,killall -9 wf-recorder; killall -9 ffplay; hyprctl keyword debug:damage_tracking 0; wf-recorder -f pipe:99 -m matroska -g "1920,0 1920x1080" -r 30 -b:v 2M -c libx264 99>&1 >&2 | ffplay -probesize 32 -sync ext -fflags nobuffer -vcodec h264 -'' else "")}
+      ${(if cfg.config.screenshare.enable then ''bind = ${cfg.config.screenshare.stopkeybind},exec,killall -9 wf-recorder; killall -9 ffplay; hyprctl keyword debug:damage_tracking ${builtins.toString (if cfg.config.debug.damage_tracking then 2 else 1)}'' else "")}
       ${(if cfg.config.screenshare.enable then ''bind = ${cfg.config.screenshare.obskeybind},exec,killall -9 ffplay; ffplay /dev/$(ls /dev/ | grep "video" | tail -1)'' else "")}
       bind = ${cfg.config.screenshot.keybind},exec,slurp | grim -g - ${cfg.config.screenshot.output_path}/$(date +'screenshot_%Y-%m-%d-%H%M%S.png'); wl-copy < ${cfg.config.screenshot.output_path}/$(ls ${cfg.config.screenshot.output_path}/ -tp | head -1)
 
