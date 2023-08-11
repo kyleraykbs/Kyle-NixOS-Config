6596b37b01e7a61e02a08286c2fdb3728fe61e41
Kyle <kyleraykbs@proton.me>
2023-08-10 21:29:20 -0700

    Added Hyprland binds to Citadel Core.

--- home/george/citadel-core.nix
+++ home/george/citadel-core.nix
@@ -49,6 +49,58 @@
     bind = $mainMod, ESCAPE, exec, kitty sh -c "cd /etc/nixos; git pull; sudo nixos-rebuild switch; hyprctl reload; echo; echo 'Press enter to exit'; read"
   '';
 
+  base.hyprland.config.screenshare.stopkeybind = "ALT SHIFT, A";
+
+  base.hyprland.config.binds = ''
+          # See https://wiki.hyprland.org/Configuring/Keywords/ for more
+          $mainMod = CTRL SHIFT
+          bind = $mainMod, RETURN, exec, kitty
+          bind = $mainMod, X, killactive,
+          bind = $mainMod, M, exit,
+          bind = $mainMod, SPACE, togglefloating,
+          bind = $mainMod, R, exec, wofi --show drun
+          bind = $mainMod, F, fullscreen, # dwindle
+          bind = $mainMod, J, togglesplit, # dwindle
+
+          # Move focus with mainMod + arrow keys
+          bind = $mainMod, left, movefocus, l
+          bind = $mainMod, right, movefocus, r
+          bind = $mainMod, up, movefocus, u
+          bind = $mainMod, down, movefocus, d
+
+          # Switch workspaces with mainMod + [0-9]
+          bind = $mainMod, 1, workspace, 1
+          bind = $mainMod, 2, workspace, 2
+          bind = $mainMod, 3, workspace, 3
+          bind = $mainMod, 4, workspace, 4
+          bind = $mainMod, 5, workspace, 5
+          bind = $mainMod, 6, workspace, 6
+          bind = $mainMod, 7, workspace, 7
+          bind = $mainMod, 8, workspace, 8
+          bind = $mainMod, 9, workspace, 9
+          bind = $mainMod, 0, workspace, 10
+
+          # Move active window to a workspace with mainMod + SHIFT + [0-9]
+          bind = ALT SHIFT, 1, movetoworkspace, 1
+          bind = ALT SHIFT, 2, movetoworkspace, 2
+          bind = ALT SHIFT, 3, movetoworkspace, 3
+          bind = ALT SHIFT, 4, movetoworkspace, 4
+          bind = ALT SHIFT, 5, movetoworkspace, 5
+          bind = ALT SHIFT, 6, movetoworkspace, 6
+          bind = ALT SHIFT, 7, movetoworkspace, 7
+          bind = ALT SHIFT, 8, movetoworkspace, 8
+          bind = ALT SHIFT, 9, movetoworkspace, 9
+          bind = ALT SHIFT, 0, movetoworkspace, 10
+
+          # Scroll through existing workspaces with mainMod + scroll
+          bind = $mainMod, mouse_down, workspace, e+1
+          bind = $mainMod, mouse_up, workspace, e-1
+
+          # Move/resize windows with mainMod + LMB/RMB and dragging
+          bindm = $mainMod, mouse:272, movewindow
+          bindm = $mainMod, mouse:273, resizewindow
+  ''
+
   services = {
     dunst = {
       enable = true;
