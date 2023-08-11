91b156aa09801b6e22f0452da748aed7594af010
Kyle <kyleraykbs@proton.me>
2023-08-10 21:45:26 -0700

    Updated HiDPI setting for Citadel Core.

--- home/george/citadel-core.nix
+++ home/george/citadel-core.nix
@@ -34,7 +34,7 @@
 
   base.hyprland.enable = true;
   base.hyprland.extraConfig = ''
-    monitor=DP-2, highres, 0x0, 2
+    monitor=DP-2, highres, 0x0, 3
 
     workspace = 1, monitor:DP-2, default:true
     workspace = 2, monitor:DP-2, default:false
