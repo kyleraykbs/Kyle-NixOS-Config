48e83157a0fd8a5e92d866d2d4bf1404c752a34a
Kyle <kyleraykbs@proton.me>
2023-08-16 17:32:35 -0700

    Added temporary openssh.

--- hosts/citadel-core/default.nix
+++ hosts/citadel-core/default.nix
@@ -22,14 +22,18 @@
 
   # services.xserver.displayManager.autoLogin.user = "george";
   # services.xserver.displayManager.autoLogin.enable = true;
-
-
+  
   services = {
     syncthing = {
         enable = true;
         user = "george";
         dataDir = "/home/george/Documents/Sync";    # Default folder for new synced folders
         configDir = "/home/george/.config/syncthing";   # Folder for Syncthing's settings and keys
+    };
+
+    openssh = {  
+      enable = true;  
+      ports = [27069];
     };
   };
 
